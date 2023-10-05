ddb23adceb0c82a8ab17dfe2862f8514647bf6d1
Ronald <ronaldcontact2019@gmail.com>
2023-10-05 20:38:36 -0300

    Ajustes no cartaoProdutos

--- Hashtag - Intensivao JavaScript/src/cartaoProduto.js
+++ Hashtag - Intensivao JavaScript/src/cartaoProduto.js
@@ -5,16 +5,17 @@ export function renderizarCatalogo() {
 
         // Criando o card
         const cartaoProduto = `
-            <div class='border-solid border-2 border-sky-500 w-48 m-2' id="card-produto-${produtoCatalogo.id}">
+            <div class='border-solid border-2 border-sky-500 w-48 m-2 flex flex-col p-2 justify-between' id="card-produto-${produtoCatalogo.id}">
             <img
             src="./assets/img/${produtoCatalogo.imagem}"
             alt="Produto 1 do Magazine Hashtag."
-            style="height: 300px"
             />
             <p class='marca'>${produtoCatalogo.marca}</p>
             <p>${produtoCatalogo.nome}</p>
             <p>$${produtoCatalogo.preco}</p>
-            <button>Adicionar</button>
+            <button class="bg-slate-950 text-slate-200 hover:bg-slate-700">
+                <i class="fa-solid fa-cart-plus"></i>
+            </button>
             </div>
         `;    
     document.getElementById("container-produto").innerHTML += cartaoProduto
